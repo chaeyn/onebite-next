dd8e8751e1c2332026c406e6029288655367cbb6
chaeyn <chaulfe@gmail.com>
2025-10-31 16:37:45 +0900

    Request Memoization

--- section04/src/app/layout.tsx
+++ section04/src/app/layout.tsx
@@ -1,6 +1,23 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { BookData } from "@/types";
+
+async function Footer() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  if (!response.ok) {
+    return <footer>제작 @chaeyn</footer>;
+  }
+  const books: BookData[] = await response.json();
+  const bookCount = books.length;
+
+  return (
+    <footer>
+      <div>제작 @chaeyn</div>
+      <div>총 도서 수: {bookCount}권</div>
+    </footer>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -15,7 +32,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @chaeyn</footer>
+          <Footer />
         </div>
       </body>
     </html>
